c26956e6582a07112421dc3fea3ec4ae757b8049
Oscar Cruz <121973352+ByMortt@users.noreply.github.com>
2023-09-13 12:59:10 -0600

    Creations and usage of bubble sort and heap sort

--- src/Ordenamientos.java
+++ src/Ordenamientos.java
@@ -26,28 +26,6 @@ public class Ordenamientos {
     }
 
     // Insertion sort
-    /*
-    from this method:
-    void insertionSort(int a[], int n){
-	int i,j,k;
-	int aux;
-
-	for(i=1; i<n; i++){
-		j=i;
-		aux=a[i];
-		while (j>0 && aux < a[j-1]){
-			a[j] = a[j-1];
-			j--;
-		}
-		a[j]=aux;
-
-
-		printf("\nIteracion numero %d \n",i);
-		printArray(a,n);
-	}
-}
-add the number of operations that the aloriythm does to the console
-     */
     public static void insertionSort(int[] array) {
         int operations = 0;
         for (int i = 1; i < array.length; i++) {
@@ -64,4 +42,85 @@ add the number of operations that the aloriythm does to the console
         }
         System.out.printf("Operaciones: %d\n", operations);
     }
+
+    /*Bubble sort
+    from this structure:
+        void bubbleSort(int a[], int size) {
+    int i, j, n;
+    n = size;
+
+    for (i = n - 1; i > 0; i--) {
+        bool swapped = false; // Flag para verificar si hubo intercambios en esta iteración
+
+        for (j = 0; j < i; j++) {
+            if (a[j] > a[j + 1]) {
+                swap(&a[j], &a[j + 1]);
+                swapped = true; // Se produjo un intercambio
+            }
+        }
+
+        if (!swapped) {
+            break; // Si no hubo intercambios, el arreglo ya está ordenado
+        }
+
+        printf("\nIteracion numero %d \n",n-i);
+		printArray(a,n);
+    }
+}
+print the number of comparisons and swaps made by the algorithm and the total number of operations (comparisons + swaps)
+
+     */
+    public static void bubbleSort(int[] array) {
+        int comparisons = 0;
+        int swaps = 0;
+        int operations;
+        int n = array.length;
+        for (int i = n - 1; i > 0; i--) {
+            boolean swapped = false;
+            for (int j = 0; j < i; j++) {
+                comparisons++;
+                if (array[j] > array[j + 1]) {
+                    Utilerias.swap(array, j, j + 1);
+                    swapped = true;
+                    swaps++;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+            System.out.println("Iteracion numero " + (n - i));
+            Utilerias.print(array);
+        }
+        operations = comparisons + swaps;
+        System.out.println("Lista ordenada");
+        Utilerias.print(array);
+        System.out.printf("Comparaciones: %d\n", comparisons);
+        System.out.printf("Intercambios: %d\n", swaps);
+        System.out.printf("Operaciones: %d\n", operations);
+    }
+
+    //Heap sort, but add the total number of operations (comparisons + swaps) that made the algorithm (includes heapyfy and buildHeap)
+    public static void heapSort(int[] array) {
+        int comparisons = 0;
+        int swaps = 0;
+        int operations;
+        int n = array.length;
+        Utilerias.buildHeap(array, n);
+        for (int i = n - 1; i > 0; i--) {
+            Utilerias.swap(array, 0, i);
+            swaps++;
+            Utilerias.heapify(array, i, 0);
+            comparisons++;
+            System.out.println("Iteracion numero " + (n - i));
+            Utilerias.print(array);
+        }
+        operations = comparisons + swaps;
+        System.out.println("Lista ordenada");
+        Utilerias.print(array);
+        System.out.println("Operaciones de heap sort");
+        System.out.printf("Comparaciones: %d\n", comparisons);
+        System.out.printf("Intercambios: %d\n", swaps);
+        System.out.printf("Operaciones: %d\n", operations);
+    }
+
 }
